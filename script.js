b595731d07ea2ed75d570668e596fe0edaeb9094
koolmilk <sbosse1409@googlemail.com>
2022-06-26 13:30:42 +0200

    Added functionality and debugged some animations

--- script.js
+++ script.js
@@ -1,4 +1,58 @@
-let url = 'https://api.giphy.com/v1/gifs/search?api_key=OaQKrnmkTw9Rd9eq2JUaH5F4CuU9kM5s&q=dog&limit=25&offset=0&rating=pg-13&lang=de';
+const API_KEY = 'OaQKrnmkTw9Rd9eq2JUaH5F4CuU9kM5s';
+
+const randomChoice = (arr) => {
+    const randIndex = Math.floor(Math.random() * arr.length);
+    return arr[randIndex];
+}
+
+const imageContainer = document.querySelector('.middle .images');
+
+const removeImage = (imagePosition) => {
+    let imageIndex = 0;
+    let loadedImages = document.querySelectorAll('img.giphy-img');
+    if(imagePosition === 'last') {
+        imageIndex = loadedImages.length-1;
+    }
+    if(loadedImages.length > 0) {
+        imageContainer.removeChild([...loadedImages][imageIndex]);
+    }
+}
+
+const searchGiphy = (searchTerm) => {
+    toggleLoading(true);
+    let url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchTerm}&limit=150&offset=0&rating=pg-13&lang=en`;
+    fetch(url)
+    .then(response => response.json())
+    .then(json => {
+        let gif = randomChoice(json.data);
+        let src = gif.images.original.url;
+
+        const image = createImage(src);
+        imageContainer.appendChild(image);
+
+        image.addEventListener('load', () => {
+            image.classList.add('visible');
+            toggleLoading(false);
+            document.body.classList.add('has-results');
+            hintElement.innerText = `Hit enter to search ${searchTerm}`;
+        })
+
+    })
+    .catch(error => {
+        console.log('Nothing here');
+    });
+}
+
+const toggleLoading = (state) => {
+    if(state) {
+        document.body.classList.add('loading');
+    } else {
+        document.body.classList.remove('loading');
+    }
+    
+}
+
+//
 
 const createVideo = src => {
     const video = document.createElement('video');
@@ -12,23 +66,43 @@ const createVideo = src => {
 const createImage = src => {
     const image = document.createElement('img');
     image.src = src;
+    image.className = 'giphy-img';
+
+/*     const randomNum = Math.random() * 25;
+    const posOrNeg = Math.random() < 0.5 ? -1 : 1;
+
+    let randomDeg = Math.floor(randomNum * posOrNeg);
+
+    image.style = `transform: rotateZ(${randomDeg}deg)`; */
 
     return image;
 }
 
-fetch(url)
-.then(response => response.json())
-.then(json => {
-    let gif = json.data[1];
-    let src = gif.images.original.url;
+//
+const searchElement = document.querySelector('.search-input');
+const hintElement = document.querySelector('.search-hint');
 
-    const image = createImage(src);
+const doSearch = (e) => {
+    const searchTerm = searchElement.value;
 
-    const imageContainer = document.querySelector('.middle .images');
-    imageContainer.appendChild(image);
+    if(searchTerm.length > 2) {
+        hintElement.innerText = `Hit enter to search ${searchTerm}`;
+        document.body.classList.add("show-hint");
+    } else {
+        document.body.classList.remove("show-hint");
+    }
+    if(e.key === 'Enter' && searchElement.value.length > 2) {
+        searchGiphy(searchTerm);
+        let loadedImages = document.querySelectorAll('img.giphy-img');
+        if(loadedImages.length > 25) {
+            removeImage();
+        }
+    }
+}
 
-    console.log(src2);
+searchElement.addEventListener('keyup', doSearch);
+document.addEventListener('keyup', e => {
+    if(e.key === 'Escape') {
+        removeImage('last');
+    }
 })
-.catch(error => {
-    console.log(error);
-});
